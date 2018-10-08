cbdc1b7d06eb47e1882457d088a47ab2ae590074
Senseinvader <d.ivantsov1983@gmail.com>
2018-10-08 22:37:42 +0200

    Create flattening algorithm for arrays

--- algorithms/alogorithms.js
+++ algorithms/alogorithms.js
@@ -23,3 +23,21 @@ let array = [1,3,5,6,10,22,33];
 console.log(binarySearch(array, 22));
 
 
+function flatteningList(list) {
+  let newArray = [];
+  flattenList (list, newArray);
+  return newArray;
+}
+
+function flattenList(list, newArray) {
+  for(element of list) {
+    if (Array.isArray(element)) {
+      flattenList(element, newArray);
+    } else {
+      newArray.push(element);
+    }
+  }
+}
+
+let multiArray = [ 1, 3, ["age", 5], "Kraków", [1, [3, [4, 5], "codecool"], 6, 7]];
+console.log(flatteningList(multiArray));
