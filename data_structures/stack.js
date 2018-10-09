3150e4ddec76ad12cad641567dd79f2b925ed05b
Senseinvader <d.ivantsov1983@gmail.com>
2018-10-09 20:11:57 +0200

    Create class Stack with push/pop methods and possibility to chaeck top element and size of stack

---
+++ data_structures/stack.js
@@ -0,0 +1,72 @@
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.size = 10;
+    this.length = 0;
+  }
+
+
+  push(value) {
+    let node = new Node(value);
+
+    if (this.top && this.length < this.size) {
+      node.next = this.top;
+      this.top = node;
+      this.length++;
+    } 
+    else if (this.top && this.length >= this.size) {
+      throw "Stack size exceeded";
+    }
+    else {
+      this.top = node;
+      this.length++;
+    }
+  }
+
+  pop() {
+
+    if(this.top && this.length > 1) {
+      this.top = this.top.next;
+      this.length--;
+    }
+    else if (this.top && length === 1) {
+      this.top = null;
+      this.length--;
+    }
+    else {
+      throw "Stack is empty";
+    }
+  }
+
+  peek() {
+    if (this.top) {
+      return this.top;
+    } else {
+      throw "Stack is empty";
+    }
+  }
+
+  size() {
+    return this.size;
+  }
+
+  freeSize() {
+    return this.size - this.length;
+  }
+}
+
+
+const stack = new Stack();
+stack.push("one");
+stack.push("two");
+console.log(stack.peek());
+stack.pop();
+console.log(stack.freeSize());
